5992e8d82677831fbfbe8ac3ffb1fec62df17b85
Brandon Spencer <brandon.spencer@gmail.com>
2017-06-02 11:05:25 -0400

    add commenting

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ document.onreadystatechange = function() {
     }
 };
 
+///sets event listener for each button
 function start() {
     //create eventhandlers for each button
     var buttons = document.getElementsByClassName("key");
@@ -23,7 +24,8 @@ function start() {
     }
 }
 
-function buttonLogic(evt) { //sorts out the button presses
+//makes decision based on which button was clicked
+function buttonLogic(evt) { 
     var btnValue = evt.target.innerHTML
 
     if ((0 <= btnValue && btnValue <= 9) || btnValue === '.') { 
@@ -42,6 +44,7 @@ function buttonLogic(evt) { //sorts out the button presses
     }
 }
 
+//Takes action depending on function pressed
 function handleFunc(btnValue){
 
 	switch (btnValue) {
@@ -69,7 +72,7 @@ function handleFunc(btnValue){
 			display.value = value2;
 			break;
 		case "%":
-			value2 = value2 /100;
+			value2 = value2 / 100;
 			display.value = value2;
 			break;
 		case "CE":
@@ -82,18 +85,18 @@ function handleFunc(btnValue){
 	checkState(btnValue);
 }
 
+// evaluates the epxression
 function evaluate() {
-	
-    total = "" + parseInt(value1) + selectedFunc + parseInt(value2);
+	total = "" + parseInt(value1) + selectedFunc + parseInt(value2);
     total = eval(total);
     value1 = value2;
     value2 = 0;
     selectedFunc = null;
     console.log(total);
     display.value = total;
-
 }
 
+//resets screen and all variables
 function clearScreen() {
     value1 = 0;
     value2 = undefined;
@@ -101,13 +104,14 @@ function clearScreen() {
     selectedFunc = null;
     console.log('Screen Cleared!');
 }
-
+//debugging function to check state of key variables - outputs to console
 function checkState(btnValue) {
 	console.log('value 1: ' + value1 + ' value 2: ' + value2);
 	console.log(btnValue);
 	console.log(selectedFunc);
 }
 
+//Avoids unnecessary leading zero in some cases
 function handleNum(btnValue) {
 	if (value2 === undefined) {
 		value2 = "" + btnValue;
